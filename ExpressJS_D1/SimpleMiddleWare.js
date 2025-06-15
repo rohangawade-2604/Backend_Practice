418957370bb88023eaaac5c160e2506bf5357846
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-16 00:25:39 +0530

    just practcing

---
+++ ExpressJS_D1/SimpleMiddleWare.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const port = 8000;
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('hello i am the middleware ')
+    next();
+    console.log('hello nextMiddleware is Here..!!!');
+})
+
+
+
+
+app.get('/', (req, res) => {
+    console.log('hello i am the welcome page');
+    res.send('welcome')
+})
+
+app.get('/about' , (req, res) => {
+    console.log("welcome to about page")
+    res.send('about')
+})
+
+app.get('/portfolio' , (req, res) => {
+    console.log("welcome to portfolio page")
+    res.send('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+    console.log('welcome to contact page');
+    res.send('contact')
+})
+
+app.listen(port, () => {
+    console.log("port is active at this point ${port}");
+})
