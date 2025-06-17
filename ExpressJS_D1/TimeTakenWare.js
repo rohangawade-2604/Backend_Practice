0a888c398b1a6945b244f3304c1a01d735ac29d3
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-17 23:43:13 +0530

    nothing

--- ExpressJS_D1/TimeTakenWare.js
+++ ExpressJS_D1/TimeTakenWare.js
@@ -5,5 +5,33 @@ const port = 8000;
 const app = express();
 
 app.use((req, res, next) => {
-    
+    const startTime = new Date().getTime();
+    next();
+    const endTime = new Date().getTime();
+    console.log(endTime - startTime);
 })
+
+
+app.get('/', (req ,res) => {
+    console.log('hello i am from base route');
+    res.send('welcome');
+})
+
+app.get('/about', (req ,res) => {
+    console.log('hello i am the about page');
+    res.send('welcome to about page');
+})
+
+app.get('/contact' , (req , res) => {
+    console.log("hello i am the contact page");
+    res.send('welcome to contact page');
+})
+
+app.get('/blogs', (req ,res) => {
+    console.log("hello i am the blogs page");
+    res.send('welcome to blogs page');
+})
+
+app.listen(port, () => {
+    console.log('server is running on port ${port}')
+});
